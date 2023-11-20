576c6eb8333164574b5e1f706bb7a44aa5862b8f
loscarpab <ccormor392@g.educaand.es>
2023-11-20 16:34:53 +0100

    hilos hechos

--- password/Program.cs
+++ password/Program.cs
@@ -1,38 +1,36 @@
 ﻿using System.Security.Cryptography;
 using System.Text;
 
-String archivoGrande = "2151220-passwords.txt";
-String archivoPequeño = "prueba-passwords.txt";
-String ruta = "..\\..\\..\\" + archivoGrande;
-
-
-
+bool encontrado = false;
 
 try
 {
+    String archivoGrande = "2151220-passwords.txt";
+    String archivoPequeño = "prueba-passwords.txt";
+    String ruta = "..\\..\\..\\" + archivoGrande;
+    int numHilos = 8;
+    
     //Leer todo el archivo
     Encoding codificacion = Encoding.UTF8;
-    string[] lines = File.ReadAllLines(ruta, codificacion);
-    Console.WriteLine("El archivo tiene: {0} líneas", lines.Length);
+    string[] todasLasLineasDelArchivo = File.ReadAllLines(ruta, codificacion);
+    Console.WriteLine("El archivo tiene: {0} líneas", todasLasLineasDelArchivo.Length);
     //escoger linea aleatoria para la contraseña a encontrar
     Random lineaElegida = new Random();
-    int numLinea = lineaElegida.Next(lines.Length);
+    int lineaAleatoriaEscogida = lineaElegida.Next(todasLasLineasDelArchivo.Length);
     //encriptar la contraseña aleatoria
-    String encrypt;
-    encrypt = Encrypt(lines[numLinea]);
-    Console.WriteLine("Texto Encriptado: " + encrypt);
+    String contraseñaEncriptada;
+    contraseñaEncriptada = Encrypt(todasLasLineasDelArchivo[lineaAleatoriaEscogida]);
+    Console.WriteLine("Texto Encriptado: " + contraseñaEncriptada);
+    
 
-
-    for (int i = 0; i < lines.Length; i++)
+    for (int iterator = 0; iterator < numHilos; iterator++)
     {
-        var lineaEncrypt = Encrypt(lines[i]);
-        if (lineaEncrypt == encrypt)
-        {
-            Console.WriteLine("La contraseña era " + lines[i]);
-            Console.WriteLine("Se encontraba en la linea {0}", i+1);
-            break;
-        }
+        int divisionLineas = (todasLasLineasDelArchivo.Length-1)/(numHilos+1);
+         
+        Thread hilo = new Thread(() => { FuerzaBruta(todasLasLineasDelArchivo, contraseñaEncriptada, divisionLineas*(iterator), divisionLineas*(iterator+1)); });
+        hilo.Start();
     }
+
 
 }
 catch (Exception e)
@@ -56,4 +54,23 @@ static string Encrypt(string contraseña)
     }
 
     return resultado;
+}
+bool FuerzaBruta(string[] lineasArchivo, string contraseñaEncriptada, int minimoLineas, int maximoLineas)
+{
+    for (int i = minimoLineas; i <= maximoLineas; i++)
+    {
+        var lineaEncrypt = Encrypt(lineasArchivo[i]);
+        if (encontrado)
+        {
+            return true;
+        }
+        if (lineaEncrypt == contraseñaEncriptada)
+        {
+            Console.WriteLine("La contraseña era " + lineasArchivo[i]);
+            Console.WriteLine("Se encontraba en la linea {0}", i + 1);
+            encontrado = true;
+            return true;
+        }
+    }
+    return false;
 }
